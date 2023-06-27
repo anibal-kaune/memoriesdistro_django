bc859846f37ee52f2ed6228e9b6676f833bee9a6
Anibal_Kaune <127349390+anibal-kaune@users.noreply.github.com>
2023-06-27 19:48:18 -0400

    desarrollo/quienessomos updated
    Falta arreglar las imágenes(no aparecen)

---
+++ memoriesdistro/tienda/templates/tienda/memoriesdistro/tienda/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    foto = models.FileField(upload_to='productos')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    id_usuario = models.IntegerField()
+
+class Usuario(models.Model):
+    correo = models.EmailField()
+    password = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100)
+
+class Cliente(models.Model):
+    correo = models.EmailField()
+    password = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100)
+
+class Boleta(models.Model):
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_de_creacion = models.DateField(auto_now_add=True)
+
+class DetalleBoleta(models.Model):
+    id_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    id_boleta = models.ForeignKey(Boleta, on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
